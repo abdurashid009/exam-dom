0a3b4771c8fb473b1ed51a3eaa19702f7813be88
Abdurashid <abdurashidnormetov2009.26@gmail.com>
2025-02-07 17:45:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+let ism = prompt('Ismingizni kiriting')
+let age = prompt('Yoshingizni kiriting');
+
+    document.getElementById("ism").textContent = ism;
+    document.getElementById("age").textContent = age;
